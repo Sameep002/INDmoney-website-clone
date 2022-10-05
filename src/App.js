25cbab1de757b5cfefe914cd9a1974ae15039601
Sameep002 <108282267+Sameep002@users.noreply.github.com>
2022-10-05 20:06:37 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-// import Ques from "./component/Ques";
-// import Cars from "./component/Cars";
-// import Hook from "./component/Hook";
-// import Reduce from "./component/Reduce"
-import Form from "./component/Form";
 import Landingpg from "./component/Indmoneyapp/Landingpg";
-import Lyrics from "./component/Lyrics";
-import Practice from "./component/Practice";
-// import Reduxcounter from "./redux/Reduxcounter";
-// import Post from "./redux/Reduxpost";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Landingpg /> */}
-      {/* <Form /> */}
-      {/* <Reduxcounter /> */}
-      {/* <Post /> */}
-      {/* <Practice /> */}
-      <Lyrics/>
+      <Landingpg />
     </div>
   );
 };
